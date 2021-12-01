287d3fac16469cd5d9200da8cd73954789a56f96
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-12-01 09:57:28 -0700

    put all sorts into one method with filled elements array...elements parameter is still in every object

---
+++ SortingTypes/src/AllSorts.java
@@ -0,0 +1,205 @@
+import java.util.Scanner;
+
+public class AllSorts
+	{
+		static int[] elements;
+		
+		//asking the questions and fill array--> elements
+		public static void questions()
+			{
+				
+				Scanner userStringInput = new Scanner(System.in);
+				System.out.println("Hello User! How many times would you like to run this simulation?");
+				int numRun = userStringInput.nextInt();
+				
+				System.out.println("How many integers would you like to sort through?");
+				int numOfNum = userStringInput.nextInt();
+				
+				int[] elements = new int[numOfNum];
+			
+				
+				for (int t : elements)
+					{
+						double doubleRando = Math.random() * 99;
+						int rando = (int)doubleRando;
+						t = rando;
+					}
+				
+				
+			}
+		
+		
+		//selection sort
+		public static void selectionSort(int[] elements)
+			{
+				
+				 long start = System.nanoTime();
+		
+			 for (int i = 0; i < elements.length - 1; i++)
+			 	{
+			 		int minIndex = i;
+			 		for (int k = i + 1; k < elements.length; k++)
+			 			{
+			 				if (elements[k] < elements[minIndex])
+			 					{
+			 						minIndex = k;
+			 					}
+			 			}
+			 		
+			 		int precious = elements[i];
+			 		elements[i] = elements[minIndex];
+			 		elements[minIndex] = precious;
+			 		}
+			 
+			 long end = System.nanoTime();
+			 long elapsedTime = end - start;
+			 System.out.println(elapsedTime);
+			}
+		
+		//insertion sort
+		public static void insertionSort(int[ ] elements)
+			{
+				long start = System.nanoTime();
+
+			 	for (int i = 1; i < elements.length; i++)
+			 		{
+			 			int temp = elements[i];
+			 			int possibleIndex = i;
+			 			while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+			 				{
+			 					elements[possibleIndex] = elements[possibleIndex - 1];
+			 					possibleIndex--;
+			 				}
+			 			elements[possibleIndex] = temp;
+			 		}
+			 	
+				 long end = System.nanoTime();
+				 long elapsedTime = end - start;
+				 System.out.println(elapsedTime);
+			}
+		
+		//quick sort
+		static int [ ] qsort(int elements[])
+			{
+				 long start = System.nanoTime();
+				 
+				qs(elements, 0, elements.length - 1);
+				 
+				 long end = System.nanoTime();
+				 long elapsedTime = end - start;
+				 System.out.println(elapsedTime);
+				return elements;
+				
+			}
+
+		private static void qs(int elements[], int left, int right)
+			{
+				int i, j;
+				int pivot, temp;
+				i = left;
+				j = right;
+				pivot = elements [(left + right) / 2];
+			
+				do
+					{
+						while ((elements [i] < pivot) && (i < right))
+							i++;
+						
+				while ((pivot < elements [j]) && (j > left))
+					{
+						j--;	
+					}
+					
+				if (i <= j)
+					{
+						temp = elements [i];
+						elements [i] = elements [j];
+						elements [j] = temp;
+						i++;
+						j--;
+					}
+					}
+				while (i <= j);
+			
+				if (left < j)
+					qs (elements, left, j);
+			
+				if (i < right)
+					qs (elements, i, right);
+			}
+		
+		//merge sort
+		private static void merge(int[] elements, int from, int mid, int to, int[] temp)
+			{
+				 long start = System.nanoTime();
+				
+				int i = from;
+			 	int j = mid + 1;
+			 	int k = from;
+			 	while (i <= mid && j <= to)
+			 		{
+			 			if (elements[i] < elements[j])
+			 				{
+			 					temp[k] = elements[i];
+			 					i++;
+			 				}
+			 			else
+			 				{
+			 					temp[k] = elements[j];
+			 					j++;
+			 				}
+			 			k++;
+			 		}
+
+			 		while (i <= mid)
+			 			{
+			 				temp[k] = elements[i];
+			 				i++;
+			 				k++;
+			 			}
+
+			 		while (j <= to)
+			 			{
+			 				temp[k] = elements[j];
+			 				j++;
+			 				k++;
+			 			}
+			 		for (k = from; k <= to; k++)
+			 			{
+			 				elements[k] = temp[k];
+			 			}
+			 		
+					 long end = System.nanoTime();
+					 long elapsedTime = end - start;
+					 System.out.println(elapsedTime);
+			}
+		
+		//bubble sort
+		static void bubbleSort(int[] elements) 
+			{  
+				 long start = System.nanoTime();
+				 
+		       int n = elements.length;  
+		       int temp = 0;  
+		       for(int i=0; i < n; i++)
+		    	   {  
+		    		   for(int j=1; j < (n-i); j++)
+		    			   {  
+		    				   if(elements[j-1] > elements[j])
+		    					   {  
+		    						   temp = elements[j-1];  
+		    						   elements[j-1] = elements[j];  
+		    						   elements[j] = temp;  
+		    					   }  
+		                          
+		    			   }  
+		    	   }  
+		  
+				 long end = System.nanoTime();
+				 long elapsedTime = end - start;
+				 System.out.println(elapsedTime);
+			}  
+		
+		
+		
+	}
--- SortingTypes/src/CoreSorter.java
+++ SortingTypes/src/CoreSorter.java
@@ -5,19 +5,30 @@ public class CoreSorter
 	
 		public static void main(String[] args)
 			{
+				AllSorts.questions();
+				AllSorts.selectionSort();
+				AllSorts.insertionSort();
+				AllSorts.qsort();
+				AllSorts.merge(); 
+				AllSorts.bubbleSort();
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 				
 				//UserInput.questions();
-				
-				
-				
-				
-				
-				
-				SelectionSort.selectionSort(int[] elements);
-				InsertionSort.insertionSort(int[] elements);
-				MergeSort.merge(int[] elements, int from, int mid, int to, int[] temp);
-				QuickSort.static int [ ] qsort(int elements[]);
-				BubbleSort.bubbleSort(int[] elements);
+//				SelectionSort.selectionSort(int[] elements);
+//				InsertionSort.insertionSort(int[] elements);
+//				MergeSort.merge(int[] elements, int from, int mid, int to, int[] temp);
+//				QuickSort.static int [ ] qsort(int elements[]);
+//				BubbleSort.bubbleSort(int[] elements);
 				
 			}
 		

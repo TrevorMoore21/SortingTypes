4d93844e95d29fd0b456a7b2bf4556ce73f4dc31
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-12-03 09:05:42 -0700

    shit

--- SortingTypes/src/FinalTimes.java
+++ SortingTypes/src/FinalTimes.java
@@ -2,4 +2,39 @@
 public class FinalTimes
 	{
 
+		private String sortName;
+		private double finalTime;
+		
+		public FinalTimes(String s, double f)
+			{
+				sortName = s;
+				finalTime = f;
+			
+			}
+		
+
+		public String getSortName()
+			{
+				return sortName;
+			}
+
+		public void setSortName(String sortName)
+			{
+				this.sortName = sortName;
+			}
+
+		public double getFinalTime()
+			{
+				return finalTime;
+			}
+
+		public void setFinalTime(double finalTime)
+			{
+				this.finalTime = finalTime;
+			}
+
+		
+		
+		
+		
 	}
---
+++ SortingTypes/src/TimeSorter.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class TimeSorter Comparator<FinalTimes>
+	{
+
+		public int compare (elapsedTime)
+		{
+			if()
+			
+		}
+		
+		
+	}
